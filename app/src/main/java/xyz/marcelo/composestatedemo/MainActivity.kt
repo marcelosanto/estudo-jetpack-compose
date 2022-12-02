d571983d5295c96246d70c57eaaacc8d93f7df19
Marcelo Santos <rocha.mer21@gmail.com>
2022-12-02 15:13:55 -0300

    mutableStateOf

--- app/src/main/java/xyz/marcelo/composestatedemo/MainActivity.kt
+++ app/src/main/java/xyz/marcelo/composestatedemo/MainActivity.kt
@@ -1,18 +1,21 @@
 package xyz.marcelo.composestatedemo
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import xyz.marcelo.composestatedemo.ui.theme.ComposeStateDemoTheme
 
@@ -21,18 +24,29 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeStateDemoTheme {
-
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    MyButton()
+                }
             }
         }
     }
 }
+
+var count = mutableStateOf(0)
 
 @Composable
 fun MyButton() {
     val context = LocalContext.current
 
     Button(
-        onClick = { /*TODO*/ },
+        onClick = {
+            count.value++
+            Toast.makeText(context, "Count is: ${count.value}", Toast.LENGTH_SHORT).show()
+        },
         contentPadding = PaddingValues(16.dp),
         border = BorderStroke(10.dp, Color.Black),
         colors = ButtonDefaults.textButtonColors(
@@ -41,7 +55,7 @@ fun MyButton() {
         )
     ) {
         Text(
-            text = "Add to cart",
+            text = "Count is: ${count.value}",
             style = MaterialTheme.typography.h3,
             modifier = Modifier.padding(5.dp)
         )
