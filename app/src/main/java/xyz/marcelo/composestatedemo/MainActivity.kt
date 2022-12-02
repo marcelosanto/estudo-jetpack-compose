6661eb057a37ec6262f1bd4ee29ba564d943374f
Marcelo Santos <rocha.mer21@gmail.com>
2022-12-02 16:03:16 -0300

    Saving The State With rememberSaveable

--- app/src/main/java/xyz/marcelo/composestatedemo/MainActivity.kt
+++ app/src/main/java/xyz/marcelo/composestatedemo/MainActivity.kt
@@ -10,7 +10,11 @@ import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -28,7 +32,8 @@ class MainActivity : ComponentActivity() {
                     verticalArrangement = Arrangement.Center,
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    var count by remember { mutableStateOf(0) }
+                    var count by rememberSaveable { mutableStateOf(0) }
+
                     MyButton(count) {
                         count++
                     }
